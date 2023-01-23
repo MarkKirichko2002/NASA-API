5dc1a1b7a7ddc20b61f9692ade09c1cf08ee4867
MarkDestroyer <markkir123@gmail.com>
2023-01-23 06:03:14 +0300

    update
    Changed NASA image categories screen.

---
+++ NASA API/NASA API/Modules/MVVM Modules/NASA Image Categories MVVM/View/NASAImageCategoriesListView.swift
@@ -0,0 +1,62 @@
+//
+//  NASAImageCategoriesListView.swift
+//  NASA API
+//
+//  Created by Марк Киричко on 23.01.2023.
+//
+
+import UIKit
+
+protocol NASAImageCategoriesListViewDelegate: AnyObject {
+    func ShowNASAimages(_ nasaImageCategoriesListView: NASAImageCategoriesListView, didSelectCategory category: NasaImageCategory)
+}
+
+final class NASAImageCategoriesListView: UIView {
+
+    public weak var delegate: NASAImageCategoriesListViewDelegate?
+    
+    private let viewModel = NASAImageCategoriesListViewViewModel()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(NASAImageCategoriesCollectionViewCell.self, forCellWithReuseIdentifier: NASAImageCategoriesCollectionViewCell.identifier)
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(collectionView)
+        addConstraints()
+        setUpCollectionView()
+        viewModel.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    private func setUpCollectionView() {
+        collectionView.dataSource = viewModel
+        collectionView.delegate = viewModel
+    }
+}
+
+extension NASAImageCategoriesListView: NASAImageCategoriesListViewViewModelDelegate {
+    func didSelectCategory(_ category: NasaImageCategory) {
+        delegate?.ShowNASAimages(self, didSelectCategory: category)
+    }
+}
