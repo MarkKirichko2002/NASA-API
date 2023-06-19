3352b3d2d74c0d2d0c0af9762d43becf75b21693
MarkDestroyer <markkir123@gmail.com>
2023-06-20 02:18:12 +0300

    update
    Added MVVM KVO in project.

---
+++ NASA API/NASA API/Resources/Clean Architecture/MVVM Modules/Asteroids MVVM KVO/View/AsteroidViewController.swift
@@ -0,0 +1,64 @@
+//
+//  AsteroidsViewController.swift
+//  NASA API
+//
+//  Created by Марк Киричко on 26.08.2022.
+//
+
+import UIKit
+
+final class AsteroidsViewController: UIViewController {
+
+    @objc private var viewModel: AsteroidsViewModel?
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: self.view.frame)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    // MARK: - Init
+    init(viewModel: AsteroidsViewModel?) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Астероиды"
+        SetUpTable()
+        bindTableView()
+        viewModel?.GetAsteroids()
+    }
+    
+    private func SetUpTable() {
+        self.view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func bindTableView() {
+        viewModel?.observation = observe(\.viewModel?.asteroids) { object, _ in
+            self.tableView.reloadData()
+        }
+    }
+}
+
+// MARK: - Init
+extension AsteroidsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.asteroidsCount() ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "название: \(viewModel?.asteroids[indexPath.row].name ?? "")"
+        return cell
+    }
+}
